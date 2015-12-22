e51152eee6f5f95f6b68960f321c1b481498171e
Nick Belzer <nickbelzer@gmail.com>
2015-12-22 15:49:50 +0100

    Changed the menu in to a top-bar from Foundation

--- menu.php
+++ menu.php
@@ -1,9 +1,13 @@
-<hr />
-<?php
-echo "<p>Dit is het menu van de pagina '$pagina'.</p>"
-?>
-<a href="index.php">Index</a>
-<a href="inschrijven.php">Inschrijven</a>
-<a href="faq.php">Veelgestelde vragen</a>
-<a href="contact.php">Contact</a>
-<hr />
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="menu">
+      <li><a href="index.php">Index</a></li>
+      <li><a href="inschrijven.php">Inschrijven</a></li>
+      <li><a href="faq.php">Veelgestelde vragen</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+<!--    --><?php
+//    echo "<p>Dit is het menu van de pagina '$pagina'.</p>"
+//    ?>
+  </div>
+</div>
