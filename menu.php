f22df735c650149a1917dc18d48529106e0e0062
Nick Belzer <nickbelzer@gmail.com>
2015-12-22 16:25:57 +0100

    Added in Nehalennia logo

--- menu.php
+++ menu.php
@@ -1,5 +1,8 @@
 <div class="top-bar">
   <div class="top-bar-left">
+    <img src="http://pr.nehalennia.nl/images/logoNehalennia.png" style="margin-left: 2em; height: 2.5em; width: auto;">
+  </div>
+  <div class="top-bar-right">
     <ul class="menu">
       <li><a href="index.php">Index</a></li>
       <li><a href="inschrijven.php">Inschrijven</a></li>
