76937e1c232395afd41fbb06f5f6e86b2355f0af
Nick Belzer <nickbelzer@gmail.com>
2015-12-22 16:26:53 +0100

    Added in rows and columns on index page

--- index.php
+++ index.php
@@ -11,28 +11,30 @@
     include 'menu.php';
     ?>
 
-    <div>
-      
-      <iframe src="slideshow/header.php" style="width: 100%; height: 300px; border: 0;" >
-          De slideshow kon niet geladen worden omdat uw browser geen iframes accepteert.
-      </iframe>
-      
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
-        lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam
-        viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent
-        et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt
-        congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed
-        arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis,
-        pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id
-        magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra
-        tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices
-        accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at
-        malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis
-        parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies.
-        Mauris vitae nisi at sem facilisis semper ac in est.
-      </p>
+    <iframe src="slideshow/header.php" style="width: 100%; height: 300px; border: 0;" >
+      De slideshow kon niet geladen worden omdat uw browser geen iframes accepteert.
+    </iframe>
+
+    <div class="row">
+
+      <div class="small-12 columns">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
+          lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam
+          viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent
+          et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt
+          congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed
+          arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis,
+          pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id
+          magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra
+          tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices
+          accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at
+          malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies.
+          Mauris vitae nisi at sem facilisis semper ac in est.
+        </p>
+      </div>
 
     </div>
 
