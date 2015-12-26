3d430de8c504a93c78708302cef94bdee4fb64e8
Omniscimus <omniscimus@icloud.com>
2015-12-26 22:18:50 +0100

    Add contact form setup

--- config.php
+++ config.php
@@ -5,5 +5,10 @@ return array(
     "mysql-port" => "3306",
     "mysql-user" => "root",
     "mysql-password" => "",
-    "mysql-database" => "reunie"
+    "mysql-database" => "reunie",
+    
+    "schoolnaam" => "Nehalennia Stedelijke Scholengemeenschap",
+    "postadres" => "Postbus 12, 3456 AB Middelburg",
+    "e-mail" => "info@nehalenniareunie.nl",
+    "telefoon" => "0123-456789"
 );
