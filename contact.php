90a65ae9a716b263b6d9149084f23bfdf062c53f
Omniscimus <omniscimus@icloud.com>
2015-12-27 11:35:07 +0100

    Remove contact form
    We'd need a SMTP account / server to send e-mails, meh.

--- contact.php
+++ contact.php
@@ -1,17 +1,7 @@
-<?php
-$input_valid = 0;
-if (is_string($_POST["email"])) {
-    $input_valid++;
-}
-if (is_string($_POST["message"])) {
-    $input_valid++;
-}
-?>
-
 <!DOCTYPE html>
 <html>
   <head>
-      <?php include 'includes.php'; ?>
+    <?php include 'includes.php'; ?>
     <title>Contact</title>
   </head>
   <body>
@@ -22,40 +12,19 @@ if (is_string($_POST["message"])) {
     ?>
 
     <div>
-        <?php $config = include 'config.php'; ?>
+      <?php $config = include 'config.php'; ?>
       <h1>Contactinformatie</h1>
-      <h4>Organisatie</h4>
+      <p>
+        U kunt contact opnemen met de organisatie van de reünie door een e-mail
+        te sturen naar het adres
+        <a href="mailto:<?php echo $config["e-mail"] ?>">
+            <?php echo $config["e-mail"] ?></a>.
+      </p>
       <p>
         <?php echo $config["schoolnaam"] ?><br />
         <?php echo $config["postadres"] ?><br />
-        e-mail: <?php echo $config["e-mail"] ?><br />
         tel. <?php echo $config["telefoon"] ?>
-      </p>
-      <?php if ($input_valid < 2): ?>
-          <h4>Stuur een bericht</h4>
-          <form method="POST" action="contact.php">
-            <?php if ($input_valid != 0): ?>
-                Vult u alstublieft alle velden in.<br />
-            <?php endif; ?>
-            Uw e-mailadres: <input type="email" name="email" /><br />
-            Onderwerp<br />
-            <input type="text" name="subject" /><br />
-            Bericht<br />
-            <input type="text" name="message" /><br />
-            <input type="submit" />
-          </form>
-      <?php else: ?>
-          <?php
-          $to = $config["e-mail"];
-          $subject = (is_string($_POST["subject"])) ? $_POST["subject"] : "";
-          $message = $_POST["message"];
-          if (mail($to, $subject, $message)) {
-              echo "<p>Bedankt voor uw bericht!</p>";
-          } else {
-              echo "<p>Er is iets foutgegaan bij het verzenden van uw bericht.</p>";
-          }
-          ?>
-      <?php endif; ?>
+      </p>      
     </div>
 
     <?php
