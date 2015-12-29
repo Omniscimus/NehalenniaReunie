30c420bd97d740132ff40f3eaa63bb0b2aac0c90
tmvandendoel <t.m.vandendoel@gmail.com>
2015-12-29 16:30:13 +0100

    Added map
    Ik heb de kaart voorlopig op deze pagina neergezet. Wat betreft de layout/css moet je maar even kijken wat handig/mooi is.

--- contact.php
+++ contact.php
@@ -3,6 +3,46 @@
   <head>
     <?php include 'includes.php'; ?>
     <title>Contact</title>
+        <script src="https://maps.googleapis.com/maps/api/js"></script>
+        <script type="text/javascript">
+        <!--
+        function initializemap()
+        {
+            var div = document.getElementById("map");
+            var neh =  new google.maps.LatLng(51.494377, 3.596289)
+            var mapoptions = {
+                    center: neh,
+                    zoom: 11,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+            var map = new google.maps.Map(div, mapoptions);
+            
+                var marker = new google.maps.Marker({
+                    position: neh,
+                    map: map,
+                    title:"SSG Nehalennia"
+                });  
+             
+                var contentString = "<a href=\"http://pr.nehalennia.nl\" target=\"_new\">SSG Nehalennia</a><br>Breeweg 71E<br>4335 AP, Middelburg<br>tel. 0123-456789";
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+             
+                google.maps.event.addListener(marker, 'click', function() {
+                  infowindow.open(map,marker);
+                });
+          }
+          //-->
+        </script>
+        <style type="text/css">
+        div#map
+        {
+            width: 700px;
+            height: 400px;
+            background-color: #DDDDDD;
+            padding: 20px;
+        }
+        </style>
   </head>
   <body>
 
@@ -29,6 +69,9 @@
         </p>      
       </div>
     </div>
+    
+    <h2>Kaart</h2>
+    <div id="map">De kaart kon helaas niet geladen worden...</div>
 
     <?php
     include 'footer.php';
