34039ddfe4c4f1a0a3a78bc544ec1266da545fa7
Nick Belzer <nickbelzer@gmail.com>
2016-01-03 19:21:50 +0100

    Added in jquery

--- includes.php
+++ includes.php
@@ -2,6 +2,7 @@
 <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 <link href="resources/css/foundation.min.css" rel="stylesheet" type="text/css">
 <link href="resources/css/reunie.css" rel="stylesheet" type="text/css">
+<script src="resources/js/jquery.min.js"></script>
 <script src="resources/js/foundation.core.min.js"></script>
 <?php
 $config = include 'config.php';
