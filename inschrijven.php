3e8955b10264aaea7b53c0f2e17bcea87cacb2f0
Nick Belzer <nickbelzer@gmail.com>
2016-01-03 20:20:44 +0100

    Adjusted the inschrijven pagina a bit to have it match the new 'fields'

--- inschrijven.php
+++ inschrijven.php
@@ -46,76 +46,74 @@ $captcha_valid = captcha_is_valid($_POST["g-recaptcha-response"]);
 
     <div class="row">
       <div class="medium-10 medium-offset-1 columns">
-        <h4>Inschrijven</h4>
+          <div class="medium-6 large-8 columns left field">
 
-          <div class="row">
-            <div class="medium-6 large-8 columns left field">
-              <p>
-                Hier kunt u zich inschrijven voor de reünie. Dit houdt een
-                aantal dingen in. blablablaaaa.
+            <h4>Inschrijven</h4>
+            <p>
+              Hier kunt u zich inschrijven voor de reünie. Dit houdt een
+              aantal dingen in. blablablaaaa.
 
-                Er is een aantal vereisten blablablaaa
-                <ul>
-                  <li>Je moet >4 jaar zijn</li>
-                  <li>Je moet van koffie houden</li>
-                  <li>Je moet ......</li>
-                </ul>
-              </p>
-            </div>
-            <div class="medium-6 large-4 columns right field">
+              Er is een aantal vereisten blablablaaa
+              <ul>
+                <li>Je moet >4 jaar zijn</li>
+                <li>Je moet van koffie houden</li>
+                <li>Je moet ......</li>
+              </ul>
+            </p>
+          </div>
+          <div class="medium-6 large-4 columns right field">
 
-              <?php if ($input_valid < 2 || !$captcha_valid): ?>
+            <?php if ($input_valid < 2 || !$captcha_valid): ?>
 
-                  <h5>Schrijf u in voor de reünie:</h5>
-                  <?php if (!$captcha_valid && $input_valid !== 0): ?>
-                      <p>
-                        U bent niet geregistreerd vanwege een verkeerd
-                        CAPTCHA-resultaat. Probeert u het alstublieft nog eens.
-                        Vergeet niet op het vakje voor 'Ik ben geen robot' te
-                        klikken.
-                      </p>
-                  <?php elseif ($input_valid !== 0): ?>
-                      <p>Gelieve alle velden in te vullen.</p>
-                  <?php endif; ?>
-                  <form action="inschrijven.php" method="POST">
-                    <fieldset class="fieldset">
-                      <label>
-                        Voornaam: <i style="color: #c40d4c">*</i>
-                        <input type="text" name="voornaam" class="name-input" />
-                      </label>
-                      <label>
-                        Achternaam: <i style="color: #c40d4c">*</i>
-                        <input type="text" name="achternaam" class="name-input" />
-                      </label>
-                      <label>
-                        Examenjaar:
-                        <input type="number" name="examenjaar" class="name-input" value="1985" />
-                      </label>
-                      <div class="g-recaptcha" data-sitekey="<?php echo $config["captcha-sitekey"] ?>"></div>
-                      <div class="padding-top-1"></div>
-                      <input type="submit" value="Versturen" class="button"
-                             style="margin-bottom: 0;"/>
-                    </fieldset>
-                  </form>
+                <h5>Schrijf u in voor de reünie:</h5>
+                <?php if (!$captcha_valid && $input_valid !== 0): ?>
+                    <p>
+                      U bent niet geregistreerd vanwege een verkeerd
+                      CAPTCHA-resultaat. Probeert u het alstublieft nog eens.
+                      Vergeet niet op het vakje voor 'Ik ben geen robot' te
+                      klikken.
+                    </p>
+                <?php elseif ($input_valid !== 0): ?>
+                    <p>Gelieve alle velden in te vullen.</p>
+                <?php endif; ?>
+                <form action="inschrijven.php" method="POST">
+                  <fieldset class="fieldset">
+                    <label>
+                      Voornaam: <i style="color: #c40d4c">*</i>
+                      <input type="text" name="voornaam" class="name-input" />
+                    </label>
+                    <label>
+                      Achternaam: <i style="color: #c40d4c">*</i>
+                      <input type="text" name="achternaam" class="name-input" />
+                    </label>
+                    <label>
+                      Examenjaar:
+                      <input type="number" name="examenjaar" class="name-input" value="1985" />
+                    </label>
+                    <div class="g-recaptcha" data-sitekey="<?php echo $config["captcha-sitekey"] ?>"></div>
+                    <div class="padding-top-1"></div>
+                    <input type="submit" value="Versturen" class="button"
+                           style="margin-bottom: 0;"/>
+                  </fieldset>
+                </form>
 
-              <?php else: ?>
+            <?php else: ?>
 
-                  <?php
-                  require_once 'MySQL_Manager.php';
-                  $mysql = new MySQL_Manager();
-                  try {
-                      $mysql->connect();
-                      $mysql->insertNewSubscription($_POST["voornaam"], $_POST["achternaam"], $_POST["examenjaar"]);
-                      $mysql->closeConnection();
-                  } catch (\Exception $e) {
-                      echo $e->getMessage();
-                  }
-                  ?>
-                  <p>Bedankt voor uw inschrijving!</p>
-              <?php endif; ?>
+                <?php
+                require_once 'MySQL_Manager.php';
+                $mysql = new MySQL_Manager();
+                try {
+                    $mysql->connect();
+                    $mysql->insertNewSubscription($_POST["voornaam"], $_POST["achternaam"], $_POST["examenjaar"]);
+                    $mysql->closeConnection();
+                } catch (\Exception $e) {
+                    echo $e->getMessage();
+                }
+                ?>
+                <p>Bedankt voor uw inschrijving!</p>
+            <?php endif; ?>
 
           </div>
-        </div>
 
       </div>
     </div>
