517c89fb64b60941c1459dc988024e93ba9db225
Nick Belzer <nickbelzer@gmail.com>
2016-01-04 20:08:31 +0100

    Changed the index page
    The text fields on the index page now overlap slightly with the images.
    The facebook icon now shows at the bottom of the page in it's own 'field'

--- index.php
+++ index.php
@@ -11,14 +11,19 @@
       include 'menu.php';
       ?>
 
-      <iframe class="slideshow field" src="slideshow/header.php" style="width: 100%; height: 300px;" >
+      <iframe class="slideshow field" src="slideshow/header.php" style="width: 100%; height: 300px; position: relative; z-index: -1;" >
         De slideshow kon niet geladen worden omdat uw browser geen iframes accepteert.
       </iframe>
 
-      <div class="row">
+      <div class="row" style="margin-top: -4em;">
         <div class="medium-10 medium-offset-1 columns">
 
-          <p class="field">
+            <div class="medium-12 left columns field">
+              <h4>Nehalennia Gymnasium Reunie</h4>
+            </div>
+
+
+          <div class="medium-6 columns field">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam
             lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam
             viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent
@@ -27,19 +32,21 @@
             arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet,
             consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis,
             pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id
+          </div>
+          <div class="medium-6 columns field">
             magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra
             tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices
             accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at
             malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis
             parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies.
             Mauris vitae nisi at sem facilisis semper ac in est.
-          </p>
+          </div>
 
-          <div style="text-align: center;">
-            <a href="<?php echo $config["facebook-link"]; ?>" target="_blank">
-              <img src="resources/img/fb.png" alt="Facebook"
-                   style="width: 10%; min-width: 114px; height: auto;"/>
-            </a>
+          <div class='medium-12 columns field' style="text-align: right;">
+              <a href="<?php echo $config["facebook-link"]; ?>" target="_blank">
+                <img src="resources/img/fb.png" alt="Facebook"
+                     style="width: 10%; min-width: 114px; height: auto;"/>
+              </a>
           </div>
 
         </div>
