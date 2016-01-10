5306d492ea6021bbb8e66dc8e0b226d1271c371c
Nick Belzer <nickbelzer@gmail.com>
2016-01-10 13:53:18 +0100

    Changed the size of the slideshow to fit the size of the text, the images look better that way

--- src/index.php
+++ src/index.php
@@ -10,12 +10,16 @@
       $pagina = "index";
       include 'resources/includes/menu.php';
       ?>
+      <div class="row" style="padding:0;">
+        <div class="medium-10 medium-offset-1 columns" style="padding:0;">
+          <iframe class="slideshow field" src="slideshow/slideshow.php" style="width: 100%; height: 300px; position: relative; z-index: -1; " >
+            De slideshow kon niet geladen worden omdat uw browser geen iframes accepteert.
+          </iframe>
+        </div>
+      </div>
 
-      <iframe class="slideshow field" src="slideshow/slideshow.php" style="width: 100%; height: 300px; position: relative; z-index: -1; " >
-        De slideshow kon niet geladen worden omdat uw browser geen iframes accepteert.
-      </iframe>
 
-      <div class="row" style="margin-top: -4em;">
+      <div class="row" style="margin-top: -25px;">
         <div class="medium-10 medium-offset-1 columns">
 
           <div class="medium-12 left columns field header">
