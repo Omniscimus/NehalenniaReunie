f3abe7efe87627b4fe1aeffb0667dad65b2cc3bd
Nick Belzer <nickbelzer@gmail.com>
2016-01-11 09:50:23 +0100

    Update warning when form not entered correctly

--- src/config/cms-config.php
+++ src/config/cms-config.php
@@ -6,16 +6,15 @@ return array(
 
   'veelgestelde-vragen' => array(
     'Is dit een vraag?' => 'Ja!',
-    'Is dit nog een vraag?' => 'Misschien...',
+'Is dit nog een vraag?' => 'Misschien...',
+
   ),
 
   'e-mail' => 'info@nehalenniareunie.nl',
   'contactgegevens' => array(
-    'Nehalennia Stedelijke Scholengemeenschap',
-    'Postbus 12, 3456 AB Middelburg',
-    'tel. 0123-456789',
+    'Nehalennia Stedelijke Scholengemeenschap','Postbus 12, 3456 AB Middelburg','tel. 0123-456789',
   ),
 
-  'homepage-tekst' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.',
+  'homepage-tekst' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.'
 
 );
