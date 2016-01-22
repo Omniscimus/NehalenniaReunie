3807a7f76c873d2069dbe9d99615a98942afc173
tmvandendoel <t.m.vandendoel@gmail.com>
2016-01-22 16:26:41 +0100

    Preloading

--- src/slideshow/slideshow.php
+++ src/slideshow/slideshow.php
@@ -12,7 +12,6 @@
             // Panels variabelen, zie init();
             var div1;
             var div2;
-
             // Bij het laden een Array met afbeeldingen laden;
             // PHP gaat verzorgen welke afbeeldingen.
             var slideArray = [];
@@ -21,43 +20,37 @@
                 // Haal . en .. eruit
                 $urls = array_slice($urls, 2);
                 shuffle($urls);
-
                 for($i=0; $i<6; $i++) {
-                  echo "slideArray[".$i."] = new Image();"; 
+                  echo "slideArray[".$i."] = new Image();\n"; 
                   echo "slideArray[".$i."].src = '../resources/slideshow-fotos/".$urls[$i]."';\n";
                 }
             ?>
-
             function init() {
                 div1 = document.getElementById("displaypanel1");
                 div2 = document.getElementById("displaypanel2");
                 transform1();
             }
-
             //De functie transform1 en transform2() wisselen elkaar af. Er zijn twee divs, waarvan de voorste afwisselend in/uit fade.
             function transform1()
             {
                 increment();
-                div2.style.backgroundImage = slideArray[i].src;//"url('" + slideArray[i] + "')";
+                div2.style.backgroundImage = "url('" + slideArray[i].src + "')";
                 div2.classList.remove("fadeout");
                 div2.classList.add("fadein");
                 div2.style.opacity = 1;
-
                 //Nieuw afbeelding na 3 sec
                 setTimeout("transform2();",displayTime);
             }
             function transform2()
             {
                 increment();
-                div1.style.backgroundImage = slideArray[i].src;//"url('" + slideArray[i] + "')";
+                div1.style.backgroundImage = "url('" + slideArray[i].src + "')";
                 div2.classList.remove("fadein");
                 div2.classList.add("fadeout");
                 div2.style.opacity = 0;
-
                 //Nieuw afbeelding na 3 sec
                 setTimeout("transform1();",displayTime);
             }
-
             function increment() {
                 i++;
                 if(i > slideArray.length - 1)
